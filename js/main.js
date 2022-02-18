4b126f4d482fa7cc2d1dc380af6319aa2d3b3b9a
arttiam <71148141+arttiam@users.noreply.github.com>
2022-02-19 00:55:53 +0500

    fuction1

--- js/main.js
+++ js/main.js
@@ -14,5 +14,5 @@ function randomNumberFload(min, max, length) {
   return (Math.random() * (max - min) + min).toFixed(length);
 }
 
-console.log('случайное целок число ' + randomNumberInt(1, 500));
-console.log("случайное число с плавующей запятой " + randomNumberFload(0, 5.5, 2));
+randomNumberInt(1, 500);
+randomNumberFload(0, 5.5, 2);
